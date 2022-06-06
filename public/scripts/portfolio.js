2c85fa341ae003daae5f2cb771ec04c0163907a7
Arik <arikabdullah1122@gmail.com>
2022-06-06 00:25:34 -0400

    Added a dropdown element to the PORTFOLIO button in the navbar that includes BOUDOIR, BRIDAL BOUDOIR, and COUPLES BOUDOIR. I also created a modal element for each of those pages respectively. I added media query responsiveness for the navbar dropdown menu for both tablet and mobile. I finished by refactoring my CSS.

--- public/scripts/portfolio.js
+++ public/scripts/portfolio.js
@@ -1,3 +1,25 @@
-$(".portfolio-img").click(function (event) {
+// Getting the modal
+
+var modal = document.getElementById("myModal");
+
+// Modal variables
+
+var img = $(".portfolio-img");
+var modalImg = document.getElementById("img01");
+
+// Getting the clicked image and putting it in the modal
+
+img.click(function (event) {
 	console.log(this);
+	modal.style.display = "block";
+	modalImg.src = this.src;
+});
+
+// Get the <span> element that closes the modal
+var span = $(".close");
+
+// When the user clicks on <span> (x), close the modal
+span.click(function (event) {
+	console.log(this);
+	modal.style.display = "none";
 });
